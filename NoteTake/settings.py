1e9fedcfd6da4ea4609cd4e33444c214286ddd0e
AyushGupta2301 <ayush2301ak@gmail.com>
2022-09-13 01:24:51 +0530

    minor

--- NoteTake/settings.py
+++ NoteTake/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'l^cib7cgu!=cu@-sb)4n^=%cp!&n#tq4$f&=1k$c37-vo!@tsj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.localhost','127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['.localhost','127.0.0.1','https://notetake2301.herokuapp.com/']
 
 
 # Application definition
